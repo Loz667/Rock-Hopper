585771da7fbe2daa442949f12239fbb34c50dbbd
Loren Penny <loren_667@hotmail.com>
2023-01-28 20:15:36 +0000

    Sounds & FX

--- Rock Hopper Prototype/Assets/_Scripts/PlayerController.cs
+++ Rock Hopper Prototype/Assets/_Scripts/PlayerController.cs
@@ -8,6 +8,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce;
     [SerializeField] private float gravityMultiplier;
 
+    [SerializeField] ParticleSystem impactEffect;
+    [SerializeField] ParticleSystem dirtParticle;
+
+    private AudioSource m_Audio;
+    [SerializeField] AudioClip jumpSound;
+    [SerializeField] AudioClip crashSound;
+
     bool IsGrounded = true;
     public bool gameOver = false;
 
@@ -15,6 +22,7 @@ public class PlayerController : MonoBehaviour
     {
         m_RB = GetComponent<Rigidbody>();
         m_Anim = GetComponent<Animator>();
+        m_Audio = GetComponent<AudioSource>();
         Physics.gravity *= gravityMultiplier;
     }
 
@@ -25,6 +33,8 @@ public class PlayerController : MonoBehaviour
             m_RB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             IsGrounded = false;
             m_Anim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            m_Audio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -33,9 +43,13 @@ public class PlayerController : MonoBehaviour
         if (other.collider.CompareTag("Ground"))
         {
             IsGrounded = true;
+            dirtParticle.Play();
         }
         else if (other.collider.CompareTag("Obstacle"))
         {
+            dirtParticle.Stop();
+            impactEffect.Play();
+            m_Audio.PlayOneShot(crashSound, 1.0f);
             gameOver = true;
             m_Anim.SetBool("Death_b", true);
             m_Anim.SetInteger("DeathType_int", 1);
